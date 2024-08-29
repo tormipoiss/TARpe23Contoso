8a4f914fe52a68d4ecbd40733c167f580eaabaf7
Tormi Laane <tormipoiss@gmail.com>
2024-08-29 16:55:53 +0300

    added students controller and students index view

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using Contoso_University.Data;
+using Contoso_University.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Controllers
+{
+	public class StudentsController : Controller
+	{
+		private readonly SchoolContext _context;
+
+		public StudentsController(SchoolContext context)
+		{
+			_context = context;
+		}
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Students.ToListAsync());
+		}
+		/*
+        public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? pageNumber
+            )
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParam"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParam"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null) 
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["currentFilter"] = searchString;
+
+            var students = from student in _context.Students
+                           select student;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(student => 
+                student.LastName.Contains(searchString) || 
+                student.FirstMidName.Contains(searchString));
+            }
+            switch (sortOrder) 
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(student => student.LastName);
+                    break;
+                case "firstname_desc":
+                    students = students.OrderByDescending(student => student.FirstMidName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(student => student.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(student => student.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(student => student.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            return View(await _context.Students.ToListAsync());
+        }
+        */
+	}
+}
--- obj/Debug/net8.0/Contoso University.AssemblyInfo.cs
+++ obj/Debug/net8.0/Contoso University.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contoso University")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457e8c146c04e05a00ff7505b472a8d5f570d814")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contoso University")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contoso University")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
